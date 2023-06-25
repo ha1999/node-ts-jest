da3f195ca8a3a742251eee8a82caf8a6e7bceb39
haln14 <haln14>
2023-06-25 21:02:01 +0700

    feat: Basic operation testing

--- src/tests/math.test.ts
+++ src/tests/math.test.ts
@@ -4,11 +4,25 @@ import { add, multiply } from "../app/math";
 describe("Math functions", () => {
   it("should multiply 5 by 3", () => {
     const result = multiply(5, 3);
-    expect(result).toEqual(15);
+    expect(result).toBe(15);
   });
 
   it("should add 5 by 3", () => {
     const result = add(5, 3);
-    expect(result).toEqual(8);
+    expect(result).toBe(8);
+  });
+
+  test('object assignment', () => {
+    const data: {one: number, two?: number} = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+  });
+
+  test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
   });
 });
